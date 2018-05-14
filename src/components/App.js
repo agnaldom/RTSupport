b85a789f2084fcd4687d460fdedb8fc279b7578c
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-05-14 18:12:53 -0300

    Adicionando a funcao da message no app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ChannelSection from './channels/ChannelSection';
 import UserSection from './users/UserSection';
+import MessageSection from './messages/MessageSection';
 
 class App extends Component {
   constructor(props){
@@ -25,20 +26,32 @@ class App extends Component {
     this.setState({users});
     // TODO: Sendo to Server
   }
+  addMessage(body){
+    let {messages, users} = this.state;
+    let createdAt = new Date;
+    let author = users.length > 0 ? users[0].name : 'anonymous';
+    messages.push({id: messages.length, body, createdAt, author});
+    this.setState({messages});
+    // TODO: Send to Server
+  }
   render() {
     return (
       <div className='app'>
          <div className='div'>
-         <ChannelSection
-          {...this.state}
-          addChannel={this.addChannel.bind(this)}
-          setChannel={this.setChannel.bind(this)}
-        />
-        <UserSection 
-          {...this.state}
-          setUserName={this.setUserName.bind(this)}
-        />
+          <ChannelSection
+            {...this.state}
+            addChannel={this.addChannel.bind(this)}
+            setChannel={this.setChannel.bind(this)}
+          />
+          <UserSection 
+            {...this.state}
+            setUserName={this.setUserName.bind(this)}
+          />
         </div>
+        <MessageSection 
+          {...this.state}
+          addMessage={this.addMessage.bind(this)}
+        />
       </div>
    
     );
